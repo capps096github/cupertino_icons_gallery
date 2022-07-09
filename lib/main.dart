97c0080f4c238a192f20b041decf3de56653854a
C APPS GITHUB TEAM <84335028+capps096github@users.noreply.github.com>
2022-07-10 02:55:46 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,3 +13,13 @@ void main() async {
     ),
   );
 }
+
+// TODO: update README.md to reflect featured on Google Dev Library
+// Post about it in group
+
+// https://devlibrary.withgoogle.com/products/flutter/repos/capps096github-cupertino_icons_gallery
+
+// authors
+// https://devlibrary.withgoogle.com/authors/capps096github
+
+// create deeplink and artwork for the app being featured on Google Dev Library
